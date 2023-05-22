837ec25145c5e2cb11f0cc5dc1ad55c865116ef7
tegarpenemuan <tegar@aksaramaya.com>
2023-05-22 16:32:45 +0700

    Add endpoint

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Your API routes and other middleware
+app.get("/api/users", (req, res) => {
+  // Your logic to retrieve users from the database or any other data source
+  const users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" },
+  ];
+
+  res.json(users);
+});
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000/api-docs");
 });
+
+// npm run generate-swagger
+// node app.js
